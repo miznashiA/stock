af001fc12dfc4aed2eb2f78ad7ed48bbaa25a0ae
miznashi <contact@nwl.jp>
2017-10-17 22:36:03 +0900

    dir change

--- stock/assets/inc/footer.php
+++
@@ -1,6 +0,0 @@
-</div><!-- /.wrap -->
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="../assets/js/common.js" defer></script>
-</body>
-</html>
