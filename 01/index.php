bef265eb01f8183d3ddb23bd07997c8eacc547ea
miznashi <contact@nwl.jp>
2017-10-17 21:41:23 +0900

    first

---
+++ 01/index.php
@@ -0,0 +1,31 @@
+<?php include (dirname(__FILE__).'/../assets/inc/header.php'); ?>
+
+<header>
+  <h1>Flex-box を使った複数行カラム、可変レイアウトの基本(SAMPLE)</h1>
+</header>
+
+<div class="contents">
+
+  <!-- sample -->
+  <ul class="flex-container">
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Ruby on Rails</li>
+    <li>javascript</li>
+    <li>PHP</li>
+  </ul>
+  <!-- sample -->
+
+  <div class="description">
+    <h2>説明</h2>
+    <p>
+      ブラウザサイズを可変させて見てください、「5 → 3 → 2 → 1」とカラム数が変化すると思います。
+      <span class="link">
+        <a href="" target="_blank">このSAMPLEの詳しい情報を見る</a>
+      </span>
+    </p>
+  </div>
+
+</div>
+
+<?php include (dirname(__FILE__).'/../assets/inc/footer.php'); ?>
